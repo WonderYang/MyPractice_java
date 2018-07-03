35a743a0294ca09287b111dbd2095743d8615300
EternalYang <1649106793@qq.com>
2018-07-03 16:40:04 +0800

    java基础

---
+++ java基础编程/RRandom.java
@@ -0,0 +1,10 @@
+package com.hanshu;
+import java.util.Random;
+public class RRandom {
+	public static void main(String[] args)
+	{
+		Random random=new Random();
+		System.out.println(random.nextInt(2));
+	}
+
+}
