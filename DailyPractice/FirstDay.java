d83decbc4d24071e5e279f2109aff725a271973b
EternalYang <1649106793@qq.com>
2018-12-22 11:15:36 +0800

    一些java的基础语句和数据类型分析

---
+++ DailyPractice/FirstDay.java
@@ -0,0 +1,35 @@
+public class FirstDay{
+    public static void main(String[] args){
+        //System.out.println(args[0]);
+
+        // byte a = 12;  //当右边的整形在byte范围之内时，就不用把12强转为byte型
+        // System.out.println(a);
+        // byte date = (byte)300;
+        // System.out.println(date);  //输出44，300的第九位被截断；
+
+        //float f = 0.1F;   //0.1后面必须加F或者f，不然会报错，精度损失；
+
+        // int num1 = 10;
+        // int num2 = 4;
+        // System.out.println(num1/num2); //2
+        // System.out.println((double)num1/num2); //2.5
+
+        // String name = "杨赟";
+        // System.out.println("my name is "+name);
+        // int a = 3;
+        // int b = 4;
+        // String result = "计算结果："+a+b;   //String类的优先级非常高，遇到加号都是拼接作用，要想得到想要的结果，必须加（）；
+        // System.out.println(result);
+        // System.out.println("3+4="+a+b);  //3+4=34   
+        // System.out.println("3+4="+(a+b));  //3+4=7
+
+        // if (1<2 | (3/0 == 0)){                //|和&都不具有短路原则
+        //     System.out.println("1ok");
+        // }
+        // if (1<2 || (3/0 == 0)){          //||和&&都具有短路原则
+        //     System.out.println("2ok");
+        // }
+
+
+    }
+}
