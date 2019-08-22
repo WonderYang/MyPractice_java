34cdd4943ffe16f62736c1f280e29d1ebcfe48de
EternalYang <1649106793@qq.com>
2019-08-22 23:11:33 +0800

    寻找最小的K个数

--- Algorithm/牛客经典算法题/Solution.java
+++ Algorithm/牛客经典算法题/Solution.java
@@ -13,7 +13,11 @@ import java.util.PriorityQueue;
  * 题目地址：https://www.nowcoder.com/questionTerminal/6a296eb82cf844ca8539b57c23e6e9bf
  * @Time : Created in 22:24 2019/8/21
  */
-//第一种解法
+/**
+ * 第一种解法，大顶堆，先选取数组的前k个数组成一个大顶堆，那么此时的堆顶即为最大元素，此时再拿剩余的数来与堆顶比较
+ * 如何比堆顶小，则替换堆顶，并进行下沉操作，如果比堆顶大，则这个数肯定不属于最小的K个数里面；
+ * 此种时间复杂度为kLogn
+ */
 public class Solution {
     public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
         int[] arr = new int[k];
@@ -63,3 +67,37 @@ public class Solution {
         arr[indexB] = temp;
     }
 }
+
+/**
+ * 第二种解法，冒泡排序法
+ */
+//public class Solution {
+//    public static void main(String[] args) {
+//        int[] arr = new int[]{1,5,78,3,10,5,7,3,-1};
+//        Solution solution = new Solution();
+//        ArrayList<Integer> list = solution.GetLeastNumbers_Solution(arr, 5);
+//        System.out.println(list);
+//    }
+//    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//        int[] arr = input;
+//        //判断是否合格，否则返回空（这个根据题意来判断的）
+//        if(input.length < k || k<0) {
+//            return arrayList;
+//        }
+//        //降序冒泡，只需要冒泡k轮即可得到最小的K个值
+//        for(int i=0; i<k; i++) {
+//            for(int j=0; j<input.length-i-1; j++) {
+//                if(arr[j] < arr[j+1]) {
+//                    int temp = arr[j];
+//                    arr[j] = arr[j+1];
+//                    arr[j+1] = temp;
+//                }
+//            }
+//        }
+//        for(int index=arr.length-1; k>0; k--,index--) {
+//            arrayList.add(arr[index]);
+//        }
+//        return  arrayList;
+//    }
+//}
